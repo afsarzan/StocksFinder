aded9c96c1025b48140d6a48f9c8edf9418ac8ee
afsarzan <afsar_zan@yahoo.com>
2026-01-06 13:53:05 -0800

    Feat: Added postgres on docker and implemented one concept

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({ component: App,
+  loader: () => {
+    return { message: 'Hello from the loader!' }
+  }
+ })
 
 function App() {
- 
-
-  return null
+  const data = Route.useLoaderData<{ message: string }>();
+  return <div> {data.message} </div>
 }
